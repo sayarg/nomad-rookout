0e41cf921c56a1c4d89e457ba44d22e2e5af0d43
Guy Sayar <guy.sayar@gmail.com>
2021-03-04 14:11:02 +0000

    make it work...

--- src/rookout/examples/HelloWorld.java
+++ src/rookout/examples/HelloWorld.java
@@ -3,15 +3,15 @@ package rootkout.examples;
 public class HelloWorld {  
     public static void main(String args[]) throws Exception {
         int count = 0;
-        volitile boolean b = true;
+        boolean b = true;
         
         while(b) {
-            foo();
+            foo(++count);
             Thread.sleep(1000);
         }
     }  
     
-    pubic static void foo() {
-        System.out.println(new java.util.Date() + " - " + ++count + " sleeping for 1 sec....");
+    public static void foo(int count) {
+        System.out.println(new java.util.Date() + " - " + count + " sleeping for 1 sec....");
     }
 }  
