4e66f4a8eef8834288d9e7facee5b37e45099df2
Guy Sayar <guy.sayar@gmail.com>
2021-03-04 09:48:41 +0000

    added counter

--- src/rookout/examples/HelloWorld.java
+++ src/rookout/examples/HelloWorld.java
@@ -2,8 +2,9 @@ package rootkout.examples;
 
 public class HelloWorld {  
     public static void main(String args[]) throws Exception {
+        int count = 0;
         while(true) {
-            System.out.println(new java.util.Date() + " - sleeping for 1 sec....");
+            System.out.println(new java.util.Date() + " - " + ++count + " sleeping for 1 sec....");
             Thread.sleep(1000);
         }
     }  
